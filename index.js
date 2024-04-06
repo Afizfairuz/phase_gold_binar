8dd34bb072652c592568aeca23c42ebb626992f8
Afizfairuz <afizahmadfairuz@gmail.com>
2024-04-06 15:24:59 +0700

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log(`testing`)
+var car = {
+    // properti
+    type:"Toyota",
+    model:"Avanza",
+    color:"Black",
+
+    // method
+    start: function(){
+         console.log("method start");
+    },     
+    drive: function(){
+        console.log("method drive");
+    },
+    brake: function(){
+        console.log("method brake");
+    },
+    stop: function(){
+        console.log("method stop");
+    }
+    
+};   
+
+console.log(car type);
+console.log(car color);
+
